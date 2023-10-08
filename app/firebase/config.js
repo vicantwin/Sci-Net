51405b2180a168b1ef2a8a4065617f8cc74e2672
TsugikuniVi <saayuj1015@gmail.com>
2023-10-08 20:04:10 +0400

    env

--- app/firebase/config.js
+++ app/firebase/config.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAmQTCr8NtKksCL4kK_y9Vid2BL6IoGTfY",
-  authDomain: "spaceapps-nasa.firebaseapp.com",
-  projectId: "spaceapps-nasa",
-  storageBucket: "gs://spaceapps-nasa.appspot.com",
-  messagingSenderId: "1000729244923",
-  appId: "1:1000729244923:web:f93ceee71da316c5276c5e",
-  measurementId: "G-1FELZR2J5E",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
